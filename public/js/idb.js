9aba1cd812eaeb27b94a17aa42dc16f6b27f71d0
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-07-27 12:48:51 -0700

    adding index database

--- public/js/idb.js
+++ public/js/idb.js
@@ -10,3 +10,45 @@ request.onupgradeneeded = function (event) {
   // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore("new_transaction", { autoIncrement: true });
 };
+
+// upon a successful
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  if (navigator.onLine) {
+    // we haven't created this yet, but we will soon, so let's comment it out for now
+    // uploadPizza();
+  }
+};
+
+request.onerror = function (event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  // access the object store for `new_pizza`
+  const moneyObjectStore = transaction.objectStore("new_transaction");
+
+  // add record to your store with add method
+  moneyObjectStore.add(record);
+}
+
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  // access your object store
+  const moneyObjectStore = transaction.objectStore("new_transaction");
+
+  // get all records from store and set to a variable
+  const getAll = moneyObjectStore.getAll();
+
+  // more to come...
+}
